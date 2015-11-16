b511fa22310ce865c291c21d3e2845ec4b6a0ae0
OhJia <jiajia0919@gmail.com>
2015-11-16 11:16:53 -0500

    add twitterBot

---
+++ twitterBot/config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+  consumer_key:         'eFC72n6lE2AivjReP0iHww0gO', 
+  consumer_secret:      'kO2eFx5NmSRNkIRQd3l4D90F3lB25ClDEskpbJ3ymrCxOlCCnE',
+  access_token:         '14537148-nHx3vqD5bS6xM3j4aPPRv4NY6tMdw88fSvPM68jT4',
+  access_token_secret:  'OY3OLSbHrN4BLorDte3RHPKn0oPVawe6vWZ2MqAza6VKV'
+}
